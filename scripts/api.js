1e636c7c9ad4cd4fc416582ddedc729d934afdce
Slav Radev <slavradev@gmail.com>
2017-06-16 14:13:01 +0300

    Revert "task 6.3"
    This reverts commit 74b6d32c982571cdba3000d773c00ab42a9fb185.

--- scripts/api.js
+++ scripts/api.js
@@ -7,11 +7,9 @@
                 creator: 0
             },
             title: "XBoss 1080",
-            description: "Modded gaming console",
             publisher: "Pesho",
             datePublished: "2017-06-04",
-            price: 100,
-            image: "./static/fuze-f1.png"
+            price: 100
         }
     ];
 
@@ -111,24 +109,5 @@
             };
         }
     });
-        // Load single advert
-    $.mockjax(function (requestSettings) {
-        if (requestSettings.url.match(/https:\/\/mock\.api\.com\/appdata\/kid_rk\/adverts\/(.+)/) &&
-            requestSettings.method === "GET") {
-            let advertId = Number(requestSettings.url.match(/https:\/\/mock\.api\.com\/appdata\/kid_rk\/adverts\/(.+)/)[1]);
-            return {
-                response: function (origSettings) {
-                    if (requestSettings.headers["Authorization"].includes("Kinvey mock_token")) {
-                        let advert = adverts.filter(a => a._id === advertId);
-                        this.responseText = advert.shift();
-                    } else {
-                        this.status = 403;
-                        this.responseText = "You are not authorized";
-                    }
-                }
-            };
-        }
-    });
-
 })();
 
